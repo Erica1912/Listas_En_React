aaa412c7f6f91a64e5fec9d7a450496134c8774a
Erica1912 <eri.ruiz@hotmail.com.ar>
2020-03-31 20:05:35 -0500

    React - Listas con index, key

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+class App extends Component {
+  render (){
+    const numbers = [1, 1, 2, 4, 5]
+    return  (
+      <div className="App">
+        <h4>Trabajando con Listas</h4>
+        {numbers.map((number, index ) => {
+          return <p key={index}>Soy el número{number}</p>
+        })}
+      </div>
+    );
+  }  
+}
+
+
+export default App;
